07d8b52c70c3562533f08d1125119793a290781d
Yokesh <24213316+nalisan007@users.noreply.github.com>
2025-09-30 15:48:01 +0530

    Added Index for Transaction & User Entity

--- src/main/java/io/expensetracker/ExpenseTracker/restApi/dto/Users.java
+++ src/main/java/io/expensetracker/ExpenseTracker/restApi/dto/Users.java
@@ -1,13 +1,13 @@
 package io.expensetracker.ExpenseTracker.restApi.dto;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 
 @Entity
